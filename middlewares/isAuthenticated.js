2e39434e92fd8208bf68a638c6244a4405266df2
Alexis Graff <alexisgraff123@gmail.com>
2023-01-21 18:02:11 -0300

    logs

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,7 +1,11 @@
+import {log} from "../utils/logger.js";
+
 export const checkAuthentication = (req, res, next) => {
+    log.info('entre al middleware')
     if (req.isAuthenticated()) {
         next();
     } else {
+        log.error('entre al error del middleware')
         res.json({
             status: false
         })
